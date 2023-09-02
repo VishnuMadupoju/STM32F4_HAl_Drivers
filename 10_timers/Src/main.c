aaec885a00882672cc41ede8a2609396c6ed57e6
VISHNU CHARY <107873892+VishnuMadupoju@users.noreply.github.com>
2023-09-02 19:32:11 +0530

    Add files via upload

---
+++ 10_timers/Src/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "stm32f4xx_hal.h"
+#include "uart.h"
+#include "adc.h"
+#include "tim.h"
+
+
+int main()
+{
+
+  HAL_Init();
+  tim_timebase_init();
+  uart_init();
+
+  while(1)
+  {
+
+  }
+}
+
+
+void HAL_TIM_PeriodElapsedCallback( TIM_HandleTypeDef *htim)
+{
+	//Do somthing
+	printf("A second just elapsed ! \n\r");
+
+}
+
+
+void  SysTick_Handler(void)
+{
+	HAL_IncTick();
+}
+
+
+
+
